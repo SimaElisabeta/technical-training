6a731bd9d36516ac76963ddd94fb1eb006c44027
Sima Elisabeta <sima.elisabeta@gmail.com>
2024-01-02 17:54:26 +0200

    updated sql constrains, added security for groups and added some demo data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,10 +1,15 @@
 {
     "name": "Estate",  # The name that will appear in the App list
     "version": "16.0",  # Version
+    "category": "Real Estate/Brokerage", # Category for security
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [
+        "security/security.xml",
         "security/ir.model.access.csv",
+
+        "data/estate.property.type.csv",
+        "data/estate.property.tag.csv",
 
         "views/estate_property_views.xml",
         "views/estate_property_type_views.xml",
@@ -12,7 +17,10 @@
         "views/estate_property_offer_views.xml",
         "views/estate_menus.xml",
         "views/extend_users_views.xml",
-
+    ],
+    "demo": [
+        "demo/demo_data.xml",
+        "demo/demo_data_offers.xml",
     ],
     "installable": True,
     'license': 'LGPL-3',
